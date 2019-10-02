ebe8850afb41f9700c6172c140aabd69900b62ae
Andy Hsieh <andy.hsieh@lexer.com.au>
2019-10-03 06:42:31 +1000

    Update bkb version to 0.1.1

--- bkb.rb
+++ bkb.rb
@@ -1,8 +1,8 @@
 class Bkb < Formula
   desc "a simple CLI to search recent buildkite builds"
   homepage "https://github.com/shoukoo/bkb"
-  url "https://github.com/shoukoo/bkb/releases/download/0.1.0/bkb-darwin-amd64"
-  version "0.1.0"
+  url "https://github.com/shoukoo/bkb/releases/download/0.1.1/bkb-darwin-amd64.tar.gz"
+  version "0.1.1"
   sha256 "46c0cda496f71ad0586864ee88a3e915a5ae3232c7060dace8ff76ab180855b1"
 
   if OS.linux? && Hardware::CPU.is_64_bit?
@@ -15,6 +15,6 @@ class Bkb < Formula
   end
 
   test do
-    assert_match "bkb v0.1.0", shell_output("#{bin}/gitin -version", 2)
+    assert_match "bkb v0.1.1", shell_output("#{bin}/bkb -version", 2)
   end
 end
