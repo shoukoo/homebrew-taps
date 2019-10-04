0ced6a7d6109a3ea13de56da680d339babb1c62f
Andy <andy.hsieh@lexer.com.au>
2019-10-04 10:54:05 +1000

    Update bkb version to 0.1.8

--- bkb.rb
+++ bkb.rb
@@ -1,13 +1,13 @@
 class Bkb < Formula
   desc "a simple CLI to search recent buildkite builds"
   homepage "https://github.com/shoukoo/bkb"
-  url "https://github.com/shoukoo/bkb/releases/download/0.1.6/bkb-darwin-amd64.tar.gz"
-  version "0.1.6"
-  sha256 "11902370b7ef3ed3214865f51d3ee523ba3b02a24120d90f38b4d501169bc679"
+  url "https://github.com/shoukoo/bkb/releases/download/0.1.8/bkb-darwin-amd64.tar.gz"
+  version "0.1.8"
+  sha256 "763800fff02bb1364c375f10ef84f3628c5c38d879422375d7dbcc931e719f8f"
 
   if OS.linux? && Hardware::CPU.is_64_bit?
     url "https://github.com/shoukoo/bkb/releases/download/0.1.6/bkb-darwin-amd64.tar.gz"
-    sha256 "11902370b7ef3ed3214865f51d3ee523ba3b02a24120d90f38b4d501169bc679"
+    sha256 "763800fff02bb1364c375f10ef84f3628c5c38d879422375d7dbcc931e719f8f"
   end
 
   def install
@@ -15,6 +15,6 @@ class Bkb < Formula
   end
 
   test do
-    assert_match "bkb v0.1.6", shell_output("#{bin}/bkb -version", 2)
+    assert_match "bkb v0.1.8", shell_output("#{bin}/bkb -version", 2)
   end
 end
